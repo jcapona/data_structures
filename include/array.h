84db91cb71a5da3a4b06df7b959e1fbdfa61cc9a
Aviral Goel <aviralgoel@rocketmail.com>
2023-10-04 19:17:14 -0700

    Add comments (#2)

--- include/array.h
+++ include/array.h
@@ -1,28 +1,42 @@
+// https://stackoverflow.com/questions/3246803/why-use-ifndef-class-h-and-define-class-h-in-h-file-but-not-in-cpp
 #ifndef ARRAY_H_
 #define ARRAY_H_
 
 #include <iostream>
-
+// template class declaration
 template <class T>
 class Array {
     public:
+        // constructor
         Array();
+        // deconstructor
         ~Array();
 
+        //  method to add elements to the dynamic array
         void push(T value);
-        T pop();
-        void set(size_t index, T value);
-        T get(size_t index);
 
+        // returns the value at the top of the array, and removes it from the array
+        T pop();
+        // inserts an element into the array at a given index, if they index is larger than the existing size of the array
+        // array is extended and filled with zeros
+        void set(size_t index, T value);
+        //fetches the value in the array at a given index
+        T get(size_t index);
+        // returns the current size of the array
         size_t size();
+        //returns the current capacity of they array
         size_t capacity();
         void print();
         int is_empty();
 
     private:
+        // the current size of the array
         size_t m_size;
+        // the current capacity of the array
         size_t m_capacity;
+        // pointer to the data of the array
         T* m_data;
+        // method to increase the size of the array
         void resize();
 };
 
--- src/array.cc
+++ src/array.cc
@@ -8,33 +8,42 @@
 #define MIN_CAPACITY 10
 #define GROWTH_FACTOR 2
 
-
+// constructor of our dynamic array
 template <class T>
 Array<T>::Array() {
+    // the dynamic array can stores at least 10 elements in the beginning
     m_capacity = MIN_CAPACITY;
+    // initial size is 0, i.e the the pointer after initialization of they array is at 0th index
     m_size = 0;
+    // equivalent to T m_data[m_capacity]; (allocates memory block on heap)
     m_data = (T*)malloc(m_capacity * sizeof(*m_data));
+    // ensuring nothing goes wrong during malloc
     if (!m_data)
         throw std::bad_alloc();
 }
-
+// deconstrcutor of our dynamic array (de-allocates memory block on heap)
 template <class T>
 Array<T>::~Array() {
     free(m_data);
 }
 
+// given a value to be inserted, inserts it the top of the array
 template <class T>
 void Array<T>::push(T value) {
+    // if current size of the array is greater or equal to maximum capacity, therefore it is time to expand the array
     if (m_size >= m_capacity)
         resize();
     *(m_data + m_size++) = value;
 }
 
+// deletes and returns the top element of the array
 template <class T>
 T Array<T>::pop() {
     return *(m_data + --m_size);
 }
 
+// insert element in the array at a given index, if index is beyond the current size, expand the array, and then insert
+// fill the indices leading upto the index with zeros.
 template <class T>
 void Array<T>::set(size_t index, T value) {
     while (index >= m_size)
@@ -42,41 +51,50 @@ void Array<T>::set(size_t index, T value) {
     *(m_data + index) = value;
 }
 
-
+// returns the element stored a given index
 template <class T>
 T Array<T>::get(size_t index) {
     return *(m_data + index);
 }
 
+//returns the current size of the array
 template <class T>
 size_t Array<T>::size() {
     return m_size;
 }
-
+// returns the current capacity of the array
 template <class T>
 size_t Array<T>::capacity() {
     return m_capacity;
 }
 
+// expands the array by growth factor, i.e if GROWTH_FACTOR = 2, each time we expand, we double the capacity.
 template <class T>
 void Array<T>::resize() {
     size_t capacity = m_capacity*GROWTH_FACTOR;
+    // realloc is slightly different than malloc, it takes current stored data, copies it into new location with bigger chunk of contiguos memory.
+    // https://www.geeksforgeeks.org/g-fact-66/
     T *tmp = (T*)realloc(m_data, capacity * sizeof(*m_data));
-    if (!tmp)
+    // call to realloc or malloc can lead to them returning null (error), so we must check for that
+    if (!tmp) //if null
         throw std::bad_alloc();
+    // reassign m_data to new pointed location, where the expanded array is stored
     m_data = tmp;
+    // new increased capacity of array
     m_capacity = capacity;
 }
 
 template <class T>
 void Array<T>::print() {
     size_t i = 0;
+    // print all the elements of the array
     while (i < m_size) {
         std::cout << "Array[" << i << "] = " << this->get(i) << "\n";
         i++;
     }
 }
 
+//returns true or false about whether the array is empty
 template <class T>
 int Array<T>::is_empty() {
     return m_size == 0;
